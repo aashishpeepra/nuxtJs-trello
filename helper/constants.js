f0206ba021a01a00d1bb27003a303ecfbdf65665
aashishpeepra <ironhack082327@gmail.com>
2021-06-10 20:03:08 +0530

    corrected reloading with localStorage issue

--- helper/constants.js
+++ helper/constants.js
@@ -1,4 +1,98 @@
 export default {
     "status":["Not Started","Started","Completed"],
-    "priority":["P1","P2","P3","P4","P5"]
+    "priority":["P1","P2","P3","P4","P5"],
+    "json":[
+        {
+          title: "Backlog",
+          boardId: 1,
+          items: [
+            {
+              title: "Backlog",
+              id: "1",
+              board: "Backlog",
+              status: "Started",
+              priority: "P2",
+              desc: ""
+            },
+            {
+              title: "Backlog2",
+              id: "2",
+              board: "Backlog",
+              status: "Started",
+              priority: "P2",
+              desc: ""
+            },
+            {
+              title: "Backlog3",
+              id: "3",
+              board: "Backlog",
+              status: "Started",
+              priority: "P2",
+              desc: ""
+            }
+          ]
+        },
+
+        {
+          title: "Progress",
+          boardId: 2,
+          items: [
+            {
+              title: "Progress",
+              id: "4",
+              board: "Progress",
+              status: "Started",
+              priority: "P2",
+              desc: ""
+            },
+            {
+              title: "Progress2",
+              id: "5",
+              board: "Progress",
+              status: "Started",
+              priority: "P2",
+              desc: ""
+            },
+            {
+              title: "Progress3",
+              id: "6",
+              board: "Progress",
+              status: "Started",
+              priority: "P2",
+              desc: ""
+            }
+          ]
+        },
+
+        {
+          title: "Completed",
+          boardId: 3,
+          items: [
+            {
+              title: "Completed",
+              id: "7",
+              board: "Completed",
+              status: "Started",
+              priority: "P2",
+              desc: ""
+            },
+            {
+              title: "Completed2",
+              id: "8",
+              board: "Completed",
+              status: "Started",
+              priority: "P2",
+              desc: ""
+            },
+            {
+              title: "Completed3",
+              id: "9",
+              board: "Completed",
+              status: "Started",
+              priority: "P2",
+              desc: ""
+            }
+          ]
+        }
+      ]
 }
--- pages/index.vue
+++ pages/index.vue
@@ -24,6 +24,7 @@
 </template>
 
 <script>
+import constants from "../helper/constants";
 import Board from "../components/board.vue";
 import localforage from "localforage";
 export default {
@@ -36,100 +37,8 @@ export default {
   beforeCreate() {
     localforage.getItem("data").then(response => {
       if (!response) {
-        localforage.setItem("data", [
-          {
-            title: "Backlog",
-            boardId: 1,
-            items: [
-              {
-                title: "Backlog",
-                id: "1",
-                board: "Backlog",
-                status: "Started",
-                priority: "P2",
-                desc: ""
-              },
-              {
-                title: "Backlog2",
-                id: "2",
-                board: "Backlog",
-                status: "Started",
-                priority: "P2",
-                desc: ""
-              },
-              {
-                title: "Backlog3",
-                id: "3",
-                board: "Backlog",
-                status: "Started",
-                priority: "P2",
-                desc: ""
-              }
-            ]
-          },
-
-          {
-            title: "Progress",
-            boardId: 2,
-            items: [
-              {
-                title: "Progress",
-                id: "4",
-                board: "Progress",
-                status: "Started",
-                priority: "P2",
-                desc: ""
-              },
-              {
-                title: "Progress2",
-                id: "5",
-                board: "Progress",
-                status: "Started",
-                priority: "P2",
-                desc: ""
-              },
-              {
-                title: "Progress3",
-                id: "6",
-                board: "Progress",
-                status: "Started",
-                priority: "P2",
-                desc: ""
-              }
-            ]
-          },
-
-          {
-            title: "Completed",
-            boardId: 3,
-            items: [
-              {
-                title: "Completed",
-                id: "7",
-                board: "Completed",
-                status: "Started",
-                priority: "P2",
-                desc: ""
-              },
-              {
-                title: "Completed2",
-                id: "8",
-                board: "Completed",
-                status: "Started",
-                priority: "P2",
-                desc: ""
-              },
-              {
-                title: "Completed3",
-                id: "9",
-                board: "Completed",
-                status: "Started",
-                priority: "P2",
-                desc: ""
-              }
-            ]
-          }
-        ]);
+        localforage.setItem("data", constants.json);
+        this.data = constants.json;
       } else {
         this.data = response;
       }
